48e6b640d95241bbf3baeeb5567fb617dfa58979
Owenli <jesus_lqq@163.com>
2018-05-16 12:54:28 +0800

    添加 24小时时间列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -23,9 +23,10 @@ Page({
   data: {
     nowTemp: '14°',
     nowWeather: '晴天',
-    nowWeatherBackground: ''
+    nowWeatherBackground: '',
+    hourlyWeather: []
   },
-  
+
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
@@ -41,6 +42,8 @@ Page({
   onLoad: function (options) {
     this.getNow()
   },
+
+  // 获取网络数据，callback是匿名函数做参数
   getNow(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
@@ -53,20 +56,8 @@ Page({
       success: res => {
         console.log(res.data)
         let result = res.data.result
-        let temp = result.now.temp
-        let weather = result.now.weather
-        
-        // 使用this.setData异步
-        this.setData({
-          nowTemp: temp + '°',
-          nowWeather: weatherMap[weather],
-          nowWeatherBackground: '/asserts/' + weather + '-bg.png'
-        })
-        // 动态设置导航栏颜色
-        wx.setNavigationBarColor({
-          frontColor: '#000000',
-          backgroundColor: weatherColorMap[weather],
-        })
+        this.setNow(result)
+        this.setHourlyWeather(result)
       },
       complete: () => {
         callback && callback()
@@ -74,46 +65,38 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  // 设置当前天气
+  setNow(result) {
+    let temp = result.now.temp
+    let weather = result.now.weather
+
+    // 使用this.setData异步
+    this.setData({
+      nowTemp: temp + '°',
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/asserts/' + weather + '-bg.png',
+    })
+    // 动态设置导航栏颜色
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather],
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  // 未来几个小时天气
+  setHourlyWeather(result) {
+    let forecast = result.forecast
+    let nowHour = new Date().getHours()
+    var hourlyWeather = []
+    for (let i = 0; i < 8; i += 1) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/asserts/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°'
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      hourlyWeather: hourlyWeather
+    })
   }
 })
