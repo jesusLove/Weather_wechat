23f8731c1290541a6d836cf298826ac582538ed9
Owenli <jesus_lqq@163.com>
2018-05-16 10:30:55 +0800

    添加 获取网络，下拉刷新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,77 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    nowTemp: '14°',
+    nowWeather: '晴天',
+    nowWeatherBackground: ''
+  },
+  
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.getNow(() => {
+      wx.stopPullDownRefresh() // 停止刷新
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getNow()
+  },
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '青岛市'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        console.log(res.data)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        
+        // 使用this.setData异步
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/asserts/' + weather + '-bg.png'
+        })
+        // 动态设置导航栏颜色
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
   },
 
   /**
@@ -42,12 +102,6 @@ Page({
     
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
 
   /**
    * 页面上拉触底事件的处理函数
